b986e0d5ac7766a2edd0c6658adf67f6bb3ae7a3
egokick <egokick@gmail.com>
2020-06-28 13:09:34 +0100

    First launch

---
+++ variables.tf
@@ -0,0 +1,28 @@
+variable "aws_region" {
+  type    = string
+  default = "eu-west-2"
+}
+
+variable "db_table_name" {
+  type    = string
+  default = "terraform-learn"
+}
+
+variable "db_read_capacity" {
+  type    = number
+  default = 1
+}
+
+variable "db_write_capacity" {
+  type    = number
+  default = 1
+}
+
+variable "tag_user_name" {
+  type = string
+}
+
+variable "lambda_name" {
+  type = string
+  default = "terraform-test"
+}
